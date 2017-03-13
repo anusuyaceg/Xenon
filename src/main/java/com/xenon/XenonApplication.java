ac4d50aa05f254813fe5f4c283dd0457fb0b559f
Anusuya Rangasamy <Anusuya Rangasamy>
2017-03-12 17:00:50 -0700

    Initial commit

---
+++ src/main/java/com/xenon/XenonApplication.java
@@ -0,0 +1,12 @@
+package com.xenon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XenonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XenonApplication.class, args);
+	}
+}
