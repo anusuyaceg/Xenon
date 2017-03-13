91f8fb3ea3365c3b767ec06a04d00eb6a1a1eeaf
Anusuya <anusuya.ceg@gmail.com>
2017-03-12 23:28:18 -0700

    adding Xen entity and XenController

---
+++ src/main/java/com/anusuya/xenon/controller/XenController.java
@@ -0,0 +1,7 @@
+package com.anusuya.xenon.controller;
+
+/**
+ * Created by anusu on 3/12/2017.
+ */
+public class XenController {
+}
---
+++ src/main/java/com/anusuya/xenon/model/Xen.java
@@ -0,0 +1,79 @@
+package com.anusuya.xenon.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * Created by anusu on 3/12/2017.
+ */
+
+@Entity
+public class Xen {
+    @Id
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column
+    private Date timestamp;
+
+    @Column
+    private Boolean isTask;
+
+    @Column
+    private Boolean isCompleted;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Boolean getTask() {
+        return isTask;
+    }
+
+    public void setTask(Boolean task) {
+        isTask = task;
+    }
+
+    public Boolean getCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(Boolean completed) {
+        isCompleted = completed;
+    }
+}
